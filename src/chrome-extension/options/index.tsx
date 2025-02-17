e57164035a80b8b5d0536d1b7aa5f8577b4354e8
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-02-16 23:28:13 -0800

    initial commit

--- src/chrome-extension/options/index.tsx
+++ src/chrome-extension/options/index.tsx
@@ -2,8 +2,8 @@ import "../global.css";
 
 const Options = () => {
   return (
-    <div className="text-5xl p-10 font-extrabold">
-      <div>This is your options page.</div>
+    <div className='text-5xl p-10 font-extrabold'>
+      <div>Kicktab Settings</div>
     </div>
   );
 };
