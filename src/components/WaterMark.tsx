68e1904b17a2cfb47a606ffacbcf58e556767028
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-02-17 21:15:32 -0800

    watermark

---
+++ src/components/WaterMark.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface WaterMarkProps {
+  text: string;
+  spinner?: boolean;
+  width?: number;
+  height: number;
+}
+
+const WaterMark = ({ text, spinner, width, height }: WaterMarkProps) => {
+  return (
+    <div className='flex flex-col items-center justify-center h-full'>
+      <img
+        src='/assets/watermark.png'
+        alt='ball-logo'
+        width={width}
+        height={height}
+        className={`${spinner ? "animate-spin" : ""}`}
+      />
+      <p className='text-white text-2xl opacity-40 mt-8'>{text}</p>
+    </div>
+  );
+};
+
+export default WaterMark;
