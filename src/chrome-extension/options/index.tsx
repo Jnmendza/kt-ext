726fb9c01887a330b18249c1c32a14c35ea335e8
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-02-17 21:04:05 -0800

    added assets and footer comp

--- src/chrome-extension/options/index.tsx
+++ src/chrome-extension/options/index.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import "../global.css";
 
 const Options = () => {
   return (
     <div className='text-5xl p-10 font-extrabold'>
       <div>Kicktab Settings</div>
+      <Footer />
     </div>
   );
 };
